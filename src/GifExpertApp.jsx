d3cf91352119face686e42c44e68490420ca2d6b
Andres Vizcaino <andruvizcaino1989@gmail.com>
2023-04-25 17:09:52 -0500

    feat: add AddCategory component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory"
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['Dragon ball'])
+  const onAddCategory = () => {
+    const newElement = 'Valorant'
+    setCategories((prevValues) => [...prevValues, newElement])
+  }
+
   return (
     <>
       <h1>GifExpertApp</h1>
+
+      <AddCategory />
+
+      <button onClick={onAddCategory}>Agregar</button>
+
+      <ol>
+        {categories.map((category) => {
+          return (
+            <li key={category}>{category}</li>
+          )
+        })}
+      </ol>
     </>
   )
 }
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export const AddCategory = () => {
+  const [inputValue, setinputValue] = useState('')
+  const onInputChange = (e) => {
+    setinputValue(e.target.value)
+  }
+
+  return (
+    <input
+      type='text'
+      placeholder='Buscar gifs'
+      onChange={onInputChange}
+      value={inputValue}
+    />
+  )
+}
